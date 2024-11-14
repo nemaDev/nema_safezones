a3b4772633e137b2f2e063002750ffd444167dbb
NemaDev <petermannfelipe@gmail.com>
2024-11-14 10:22:16 -0300

    up

--- config_npcs.lua
+++ config_npcs.lua
@@ -15,7 +15,7 @@ ConfigNPC.SafeZonesNpc = {
             weapons = {"WEAPON_ASSAULTRIFLE", "WEAPON_CARBINERIFLE", "WEAPON_COMBATMG"},
             invincible = true,
             freeze = true,
-            respawn = 20000, -- 1 * 60000 = 1 minute
+            respawn = 1 * 60000, -- 1 * 60000 = 1 minute
             positions = {
                 vector4(431.13, -978.57, 29.71, 90.26),
                 vector4(430.88, -985.67, 29.71, 90.69),
@@ -37,7 +37,7 @@ ConfigNPC.SafeZonesNpc = {
             weapons = {"WEAPON_ASSAULTRIFLE", "WEAPON_CARBINERIFLE", "WEAPON_COMBATMG"},
             invincible = true,
             freeze = true,
-            respawn = 20000, -- 1 * 60000 = 1 minute
+            respawn = 1 * 60000, -- 1 * 60000 = 1 minute
             positions = {
                 vector4(297.24, -586.6, 42.26, 73.11), --npc 1 hospital
                 vector4(298.75, -581.22, 42.26, 81.63), --npc 1 hospital
